89e57b7c26dae262b5c2f9f8c4bf85c1b64f05b1
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-19 16:03:47 +0530

    Initial Commit

---
+++ src/components/NotAvailable.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotAvailable() {
+  return (
+    <h1 className="not-available">
+      No Movies avaialble for the selected Genre. Please select a different
+      Genre.
+    </h1>
+  );
+}
